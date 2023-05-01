5176aeb7ef18ae23be236f0624d6aa53ee3a0ee7
Henry Bugajski <henry.bug.dev@gmail.com>
2023-05-01 18:32:02 -0400

    chore: configure server

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,11 @@ app.get('/', (_, res) => {
 payload.init({
   secret: process.env.PAYLOAD_SECRET,
   mongoURL: process.env.MONGODB_URI,
+  mongoOptions: {
+    user: process.env.MONGODB_USERNAME,
+    pass: process.env.MONGODB_PASSWORD,
+    dbName: process.env.MONGODB_DATABASE,
+  },
   express: app,
   onInit: () => {
     payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);

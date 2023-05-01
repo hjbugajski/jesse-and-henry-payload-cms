5b81d034014596417b2add9cf58e878ae5d564d4
Henry Bugajski <henry.bug.dev@gmail.com>
2023-05-01 18:32:19 -0400

    chore: configure payload config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,18 +2,19 @@ import path from 'path';
 
 import { buildConfig } from 'payload/config';
 
+import Pages from './collections/Pages';
 import Users from './collections/Users';
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
   admin: {
     user: Users.slug,
   },
-  collections: [Users],
+  collections: [Pages, Users],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
+  cors: [process.env.MONGODB_IP].filter(Boolean),
 });
